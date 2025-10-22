3fc13ecb0572f69e02364ee8e05c920672f59f21
MisterHF <h.florianpro@gmail.com>
2025-10-22 17:15:39 +0200

    Setup input system player

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Script.Camera
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public float moveSpeed = 5f;
+        public float lookSpeed = 2f;
+
+        private Vector2 lookDelta;
+        private Vector2 moveInput;
+        private bool isAscending = false;
+        private bool isDescending = false;
+        private bool isRightMouseDown = false;
+
+        public static bool IsRightMouseDown;
+
+        private InputSystem_Actions actions;
+
+        private void OnEnable()
+        {
+            actions = new InputSystem_Actions();
+            actions.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+            actions.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+            actions.Player.Look.performed += ctx => lookDelta = ctx.ReadValue < Vector2>();
+            actions.Player.Move.canceled += ctx => lookDelta = Vector2.zero;
+            actions.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            actions.Player.Move.performed -= ctx => moveInput = ctx.ReadValue<Vector2>();
+            actions.Player.Move.canceled -= ctx => moveInput = Vector2.zero;
+            actions.Player.Look.performed -= ctx => lookDelta = ctx.ReadValue<Vector2>();
+            actions.Player.Move.canceled -= ctx => lookDelta = Vector2.zero;
+            actions.Player.Disable();
+        }
+
+
+        private void Update()
+        {
+
+        }
+
+    }
+}
